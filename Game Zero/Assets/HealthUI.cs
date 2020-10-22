9ecbc0b3a85c9230cc3b8695b71fc2bfc8205cfd
Zeshan Rasul <zeshanrasul@gmail.com>
2020-10-22 14:42:26 +0100

    New health UI and updated score UI

--- Game Zero/Assets/HealthUI.cs
+++ Game Zero/Assets/HealthUI.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreUI : MonoBehaviour
+public class HealthUI : MonoBehaviour
 {
     public PlayerStats playerStats;
-    Text score;
+    Text playerHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
-        score = GetComponent<Text>();
+        playerHealth = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text = "Score: " + playerStats.currentScore.ToString();
+        playerHealth.text = "Health: " + playerStats.currentHealth.ToString();
     }
 }

7bfec683b40c66bb681525de6a247e16875afd9f
Zeshan Rasul <zeshanrasul@gmail.com>
2020-10-29 13:35:42 +0000

    Functioning aiming rotation with comments for info

--- Game Zero/Assets/AimScript.cs
+++ Game Zero/Assets/AimScript.cs
@@ -16,12 +16,15 @@ public class AimScript : MonoBehaviour
     Transform chest;
 
     ThirdPersonMovement controller;
+
+    ClampPosition clampScript;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
         chest = animator.GetBoneTransform(HumanBodyBones.Chest);
         controller = GetComponent<ThirdPersonMovement>();
+        clampScript = playerLookAtObject.GetComponent<ClampPosition>();
         
     }
 
@@ -46,9 +49,42 @@ public class AimScript : MonoBehaviour
         // if (playerLookAtObject.rotation.eulerAngles.y < 90 && playerLookAtObject.rotation.eulerAngles.y > -90)    
         // {
         // }
+        // clampScript.isBehindPlayer != true
         if (controller.isAiming && !controller.isRunning)
         {
-            chest.LookAt(playerLookAtObject);
+            // chest.LookAt(playerLookAtObject);
+            
+            // if (chest.rotation.eulerAngles.y == 90 && clam)
+            Vector3 direction = (playerLookAtObject.position - transform.position).normalized;
+            // float directionX;
+            // float directionZ = Mathf.Clamp(direction.z, -1f, 1f);
+            // Debug.Log(direction);
+            // if (direction.x > 1f)
+            // {
+            //     directionX = 1f;
+            // }   else if (direction.x < -1f)
+            // { 
+            //     directionX = -1f;
+            // } else 
+            // {
+            //     directionX = direction.x;
+            // }
+
+            // Vector3 newDirection = new Vector3(directionX, direction.y, directionZ);
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            if (lookRotation.eulerAngles.y > 90f && lookRotation.eulerAngles.y < 270f)
+            {
+                chest.SetPositionAndRotation(chest.transform.position, lookRotation);
+            }
+            if (lookRotation.eulerAngles.y > 0f && lookRotation.eulerAngles.y <=90)
+            {
+                chest.SetPositionAndRotation(chest.transform.position, Quaternion.Euler(lookRotation.eulerAngles.x, 90f, lookRotation.eulerAngles.z));
+            }
+            if (lookRotation.eulerAngles.y >= 270f && lookRotation.eulerAngles.y < 360f)
+            {
+                chest.SetPositionAndRotation(chest.transform.position, Quaternion.Euler(lookRotation.eulerAngles.x, 270f, lookRotation.eulerAngles.z));
+            }
+
         }
     }
 
--- Game Zero/Assets/ClampPosition.cs
+++ Game Zero/Assets/ClampPosition.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ClampPosition : MonoBehaviour
 {
     public GameObject player;
+
+    public bool isBehindPlayer = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,28 @@ public class ClampPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z >= player.transform.position.z)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
-        }
+        // if (transform.position.z >= player.transform.position.z)
+        // {
+        //     transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+        // }
+
+        // Vector3 toPlayer = (transform.position - player.transform.position).normalized;
+
+        // if (Vector3.Dot(toPlayer, -transform.forward) < 0)
+        // {
+        //     Debug.Log("Target behind player");
+        //     transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+        // }
+
+        // Vector3 directionToTarget = (transform.position - player.transform.position).normalized;
+        // float angle = Vector3.Angle(-player.transform.forward, directionToTarget);
+        // if (angle > 90 || angle < -90) 
+        // {
+        //     Debug.Log("Target behind player");
+        //     isBehindPlayer = true;
+        // } else 
+        // {
+        //     isBehindPlayer = false;
+        // }
     }
 }

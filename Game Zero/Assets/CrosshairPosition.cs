2c17e8e6cd5f469b101b25c59f915fef2810bdd0
Zeshan Rasul <zeshanrasul@gmail.com>
2020-10-29 16:33:28 +0000

    WIP Crosshair

---
+++ Game Zero/Assets/CrosshairPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairPosition : MonoBehaviour
+{
+    public Transform crosshairTarget;
+
+    RectTransform rect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.position = crosshairTarget.position;
+        // new Vector2 (crosshairTarget.position.x, crosshairTarget.position.y);
+    }
+}

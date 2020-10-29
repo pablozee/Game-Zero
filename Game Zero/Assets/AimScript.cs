2def01801059af24f724f6a06f0d3b09e3bb7dab
Zeshan Rasul <zeshanrasul@gmail.com>
2020-10-29 13:38:39 +0000

    Code cleanup

--- Game Zero/Assets/AimScript.cs
+++ Game Zero/Assets/AimScript.cs
@@ -6,8 +6,6 @@ public class AimScript : MonoBehaviour
 {
     public Transform playerLookAtObject;
 
-    // public Transform selfRotationCheck;
-
     public Vector3 offset;
 
     public float yRotation; 
@@ -17,60 +15,21 @@ public class AimScript : MonoBehaviour
 
     ThirdPersonMovement controller;
 
-    ClampPosition clampScript;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
         chest = animator.GetBoneTransform(HumanBodyBones.Chest);
-        controller = GetComponent<ThirdPersonMovement>();
-        clampScript = playerLookAtObject.GetComponent<ClampPosition>();
-        
+        controller = GetComponent<ThirdPersonMovement>();        
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        
-        // float yRotation = Mathf.Clamp(playerLookAtObject.rotation.eulerAngles.y, -90, 90);
-        // float yRotation = Mathf.Clamp(selfRotationCheck.transform.rotation.eulerAngles.y, 90f, 270f);
-        // yRotation = selfRotationCheck.transform.rotation.eulerAngles.y;
-        // if (yRotation > 70f && yRotation < 135f)
-        // {
-        //     yRotation = 70f;
-        // } 
-        // if (yRotation < 290f && yRotation >= 135f)
-        // {
-        //     yRotation = 290f;
-        // }
-        // Debug.Log(yRotation);
-        // // Quaternion.Euler(playerLookAtObject.eulerAngles.x, yRotation, playerLookAtObject.eulerAngles.z)
-        // chest.SetPositionAndRotation(chest.transform.position, Quaternion.Euler(playerLookAtObject.eulerAngles.x, yRotation, playerLookAtObject.eulerAngles.z));
-        // if (playerLookAtObject.rotation.eulerAngles.y < 90 && playerLookAtObject.rotation.eulerAngles.y > -90)    
-        // {
-        // }
-        // clampScript.isBehindPlayer != true
         if (controller.isAiming && !controller.isRunning)
         {
-            // chest.LookAt(playerLookAtObject);
-            
-            // if (chest.rotation.eulerAngles.y == 90 && clam)
             Vector3 direction = (playerLookAtObject.position - transform.position).normalized;
-            // float directionX;
-            // float directionZ = Mathf.Clamp(direction.z, -1f, 1f);
-            // Debug.Log(direction);
-            // if (direction.x > 1f)
-            // {
-            //     directionX = 1f;
-            // }   else if (direction.x < -1f)
-            // { 
-            //     directionX = -1f;
-            // } else 
-            // {
-            //     directionX = direction.x;
-            // }
 
-            // Vector3 newDirection = new Vector3(directionX, direction.y, directionZ);
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             if (lookRotation.eulerAngles.y > 90f && lookRotation.eulerAngles.y < 270f)
             {
